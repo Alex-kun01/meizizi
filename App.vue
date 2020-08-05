5510f798da9264ca56e1a88e79307080fbb269b2
Alex-kun01 <1790557086@qq.com>
2020-08-05 09:56:00 +0800

    IOS端取消热更新

--- App.vue
+++ App.vue
@@ -7,54 +7,60 @@
 				newVersion: '',// 最新版本
 				isUpdate: false, // 是否存在更新
 				updateUrl: '', // 
-				test: 'ooo'
+				test: 'ooo',
+				target: ''
 			}
 		},
 		methods:{
 			
 			getData(target){
 				let _this = this
-				let type = target == 'android' ? 1 : 2
-				let datas = {type: type}
-				uni.request({
-					url: this.$http + '/api/index/edition',
-					method: 'post',
-					data: datas,
-					success(res){
-						console.log('版本更新数据', res)
-						if(res.data.status === 200){
-							// 请求成功
-							let newVersion = res.data.data.version // 获取到的版本号
-							_this.newVersion = newVersion
-							if(_this.version == newVersion){
-								// 版本一致
-								_this.versioninfo = '已是最新版本'
-								_this.isUpdate = false
-								_this.test = '版本一致'
+				let datas = {type: 1}
+				if(target == 'android'){
+					uni.request({
+						url: this.$http + '/api/index/edition',
+						method: 'post',
+						data: datas,
+						success(res){
+							console.log('版本更新数据', res)
+							if(res.data.status === 200){
+								// 请求成功
+								let newVersion = res.data.data.version // 获取到的版本号
+								_this.newVersion = newVersion
+								if(_this.version == newVersion){
+									// 版本一致
+									_this.versioninfo = '已是最新版本'
+									_this.isUpdate = false
+									_this.test = '版本一致'
+								}else{
+									// 版本不一致
+									_this.versioninfo = '下载更新'
+									_this.isUpdate = true
+									_this.updateUrl = res.data.data.url
+									_this.test = '版本不一致'
+									uni.showModal({
+										title: '版本更新提示',
+										content: '检测到新的版本，请更新！' + '当前版本:' + _this.version  + '新版本:' + newVersion ,
+										success() {
+											// 更新
+											plus.runtime.openURL(_this.updateUrl)
+										}
+									})
+								}
 							}else{
-								// 版本不一致
-								_this.versioninfo = '下载更新'
-								_this.isUpdate = true
-								_this.updateUrl = res.data.data.url
-								_this.test = '版本不一致'
+								// 
 								uni.showModal({
-									title: '版本更新提示',
-									content: '检测到新的版本，请更新！' + '当前版本:' + _this.version  + '新版本:' + newVersion ,
-									success() {
-										// 更新
-										plus.runtime.openURL(_this.updateUrl)
-									}
+									title: '提示',
+									content: '请求服务器出错,请稍后再试...'
 								})
 							}
-						}else{
-							// 
-							uni.showModal({
-								title: '提示',
-								content: '请求服务器出错,请稍后再试...'
-							})
 						}
-					}
-				})
+					})
+				}else{
+					return
+				}
+				
+				
 			},
 		},
 		onLaunch: function() {
@@ -67,8 +73,8 @@
 				console.log('应用版本号',wgtinfo.version);//应用版本号
 				this.version = wgtinfo.version
 				// 获取设备类型 安卓 ios
-				let target = uni.getSystemInfoSync().platform
-				this.getData(target)
+				this.target = uni.getSystemInfoSync().platform
+				this.getData(this.target)
 			})
 		},
 		
